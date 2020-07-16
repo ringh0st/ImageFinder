52dc2ab657c3ffee54c5a5f963ab8383d7382b2d
ringhost <ringhost@gmail.com>
2020-07-16 19:16:42 +0300

    pushed to gitlab

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,7 +18,7 @@ class SearchBar extends React.Component{
                 <form 
                 onSubmit={this.onFormSubmit}className="ui form">
                     <div className="field">
-                        <label>Image Search</label>
+                        <label>Image Search:</label>
                         {/* לא שמים סוגריים אחרי שמעבירים את הקולבק פנקשיין כדי לא לקרוא לה כל פעם שמבצעים רנדר בקומפוננטה */}
                         <input 
                         autocomplete="on"
