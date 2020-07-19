b8500edb2e09078aeba7871804e3ede33b8b8f64
ringhost <ringhost@gmail.com>
2020-07-19 21:52:39 +0300

    added styled component to ImageList

---
+++ src/components/ImagesList/ImagesList.Style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.img`
+  display: grid;
+  grid-template-columns: 50px 50px 50px 50px;
+  grid-template-rows: auto;
+  grid-template-areas: 
+
+`
--- src/components/ImagesList/ImagesList.js
+++ src/components/ImagesList/ImagesList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import {ImageContainer} from './ImagesList.Style'
 const ImagesList = (props) =>{
     const images = props.images.map(({id, description,urls} )=>{
-        return <img style={{width:'40vw'}} key={id} alt={description} src={urls.regular}/>;
+        return <ImageContainer style={{width:'40vw'}} key={id} alt={description} src={urls.regular}/>;
     });
     
     return <div > {images}</div>
