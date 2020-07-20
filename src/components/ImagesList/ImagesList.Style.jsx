b0bc1285dd2c0e6d90c6714398eec1a269f9bba1
ringhost <ringhost@gmail.com>
2020-07-20 23:28:26 +0300

    changed grid gap and add componentDidMount to find the height of the images

--- src/components/ImagesList/ImagesList.Style.jsx
+++ src/components/ImagesList/ImagesList.Style.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ImageContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-gap:10px;
-  grid-auto-rows:150px;
+  grid-gap:0 10px;
+  grid-auto-rows:10px;
   
 `
